ea846266b5d791fc822d6c1e0811250280a91d8a
nilante <nila.noha@gmail.com>
2023-04-13 07:27:56 -0700

    Write a function that creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,9 +2,9 @@
 #include "main.h"
 /**
  * array_range - a function that creates an array of integers
- * @min: minimum  values 
- * @max: maximum  values 
- * Return:NULL
+ * @min: minimum  values
+ * @max: maximum  values
+ * * Return:NULL
  */
 int *array_range(int min, int max)
 {
