34cd378fb8eda99024ed987c961ac98e62c09c45
nilante <nila.noha@gmail.com>
2023-04-17 13:16:29 -0700

    puppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data of the dog
+ * @name: the name of th puppy 
+ * @age: the age of th puppy 
+ * @owner: the owner of th puppy 
+ *
+ * Description: Longer description
+ */
+struct dog
+        {
+                char *name;
+                float age;
+                char *owner;
+        };
+typedef struct dog carlin;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+carlin*pulldog (char *name, float age, char *owner);
+void free_dog(carlin *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif
