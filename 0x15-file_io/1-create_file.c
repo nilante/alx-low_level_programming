3c5ccacec081f21095d77cf256863ad1e6109778
nilante <nila.noha@gmail.com>
2023-05-09 17:03:19 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			y++;
 	}
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 x = write(file, text_content, y);
 
 	if (file == -1 || x == -1)
